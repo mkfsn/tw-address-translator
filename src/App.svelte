ec67af43daa39172a11ffe0cf79130f8d4b50646
Pei-Ming Wu <p408865@gmail.com>
2022-08-28 16:18:03 +0800

    feat: add FE page using svelte

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import {Container, Alert, Input, Button, Row, Col} from 'sveltestrap';
+</script>
+
+<main>
+	<Container>
+		<Alert color="primary">
+			<h4 class="alert-heading text-capitalize">Taiwan Address Translator</h4>
+			<Row>
+				<Col md="11">
+					<Input
+						type="address"
+						name="address"
+						placeholder="address"
+					/>
+				</Col>
+				<Col md="1">
+					<Button block>Submit</Button>
+				</Col>
+			</Row>
+		</Alert>
+	</Container>
+
+</main>
+
+<style>
+	@import "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css";
+
+	main {
+		padding-top: 24px;
+	}
+</style>
